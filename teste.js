35f4513839b9a3ca8d4f2dcf6f280da5559fa1bf
Danilo <danilo.justino1@gmail.com>
2023-01-16 23:15:07 -0300

    Create directory

--- teste.js
+++
@@ -1,7 +0,0 @@
-var watermark = require('jimp-watermark');
-var options = {
-    'ratio': 0.6,// Should be less than one
-    'opacity': 0.6, //Should be less than one
-    'dstPath' : './watermark.jpg'
-};
-watermark.addWatermark('profile.jpg', 'logo.png', options);
